8ae2874337cd3b62a4b0a277c795120455e4be56
George Somerville <georgesomerville1992@gmail.com>
2015-03-12 20:51:08 +0000

    fixed silly problem with name files

---
+++ public/app/app.js
@@ -0,0 +1,14 @@
+angular.module('app',['ngResource','ngRoute']);
+
+angular.module('app').config(function($routeProvider,$locationProvider){
+	$locationProvider.html5Mode(true);
+	// define routes
+	$routeProvider
+		.when('/',{templateUrl:'/partials/main', controller:'mainController'})
+})
+
+// create quick controller to verify app works
+
+angular.module('app').controller('mainCtrl',function($scope){
+	$scope.myVar = "hi Angular"
+})
--- server.js
+++ server.js
@@ -1,17 +1,41 @@
-var express = require('express');
-
+var express = require('express'),
+	stylus = require('stylus'),
+	logger = require('morgan'),
+	bodyParser = require('body-Parser');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 var app = express() // run express whic comes from the requre express
 
+function compile(str,path){
+	return stylus(str).set('filename',path);
+}
+
+
 // config view engine
 app.set('views', __dirname + '/server/views');
 app.set('view engine','jade');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+app.use(stylus.middleware(
+	{
+		src: __dirname + '/public',
+		compile:compile
+	}
+));
+// set up static middleware
+// this is static router handling.
+app.use(express.static(__dirname + '/public'));
 
-app.get('*',function(req, res){
+app.get('/partials/:partialPath', function(req,res){
+	res.render('partials/'+ req.params.partialPath);
+})
+
+app.get('/',function(req, res){
 	res.render('index');
-}) 
+});
+
 // any other request will be handled to this route
 // this will get serverd up to the client and we'll make the clients reponsablity to 
 	// to deal with 404 errors.
